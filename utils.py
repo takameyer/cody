b3d5278287cb654985c5e8f4ca220a9419a2812e
Andrew Meyer <andrew.meyer@mongodb.com>
2024-11-24 09:45:08 +0100

    Create chat function and update CLI options
    * Create a streaming conversation dialogue with memory
    
    * Create chat function and refactor arguments
    
    * Now opens a chat given a directory and initial prompt
    * Arguments are now a yaml config or option based

--- utils.py
+++ utils.py
@@ -1,48 +1,57 @@
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import DirectoryLoader
-
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 import os
 
-
-def load_documents():
-    loader = DirectoryLoader("./.test/kitchensink/", glob="**/*[!png|jpg|gif]")
-    docs = loader.load()
-    return docs
+# Define a new graph
 
 
-# Initialize the OpenAI LLM
-def initialize_llm():
-    return ChatOpenAI(
+def open_chat(project_path, prompt):
+    def call_model(state: MessagesState):
+        prompt_template = PromptTemplate(
+            template=(
+                prompt
+                + "\n\nYou will be prompted about a project which is provided in the context.\n\n"
+                "{context}"
+            ),
+        )
+        cody_chain = prompt_template | model
+        response = cody_chain.invoke([{"context": docs}, state["messages"]])
+        return {"messages": response}
+
+    model = ChatOpenAI(
         model_name="gpt-4o",
         temperature=0,
         openai_api_key=os.getenv("OPENAI_API_KEY"),
     )
+    loader = DirectoryLoader(project_path, glob="**/*[!png|jpg|gif]")
+    docs = loader.load()
 
+    # Define the (single) node in the graph
+    workflow = StateGraph(state_schema=MessagesState)
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
 
-# Analyze the file and generate a summary
-def analyze_file(code):
-    llm = initialize_llm()
-    docs = load_documents()
-    prompt = PromptTemplate(
-        input_variables=["code"],
-        template=(
-            "You are an expert JBoss and Spring Boot application developer. Given the context.  Tell me about the controllers and how they work.  Also tell me what dependencies are in the pom.xml:\n\n"
-            "{context}"
-        ),
-    )
-    cody_chain = prompt | llm
-    return cody_chain.invoke({"context": docs})
+    # Add memory
+    memory = MemorySaver()
+    app = workflow.compile(checkpointer=memory)
 
+    config = {"configurable": {"thread_id": "abc123"}}
 
-# Annotate the code with comments explaining its functionality
-def annotate_code(code):
-    llm = initialize_llm()
-    prompt = PromptTemplate(
-        input_variables=["code"],
-        template=(
-            "You are an expert programmer. Add comments to the following code to explain its functionality:\n\n"
-            "{code}"
-        ),
-    )
-    return llm.invoke(prompt.format(code=code))
+    print("Conversation has been opened.  Type 'quit' to leave\n")
+
+    while True:
+        query = input(">>> ")
+        if query == "quit":
+            break
+        input_messages = [HumanMessage(query)]
+        for chunk, metadata in app.stream(
+            {"messages": input_messages}, config, stream_mode="messages"
+        ):
+            if isinstance(chunk, AIMessage):
+                for byte in chunk.content:
+                    print(byte, end="", flush=True)
+        print("\n")
